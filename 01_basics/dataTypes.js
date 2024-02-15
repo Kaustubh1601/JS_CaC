cb782bc607acf3cf332221f0bdf26e906bab05f6
Kaustubh Mishra <83139466+Kaustubh1601@users.noreply.github.com>
2024-02-15 07:50:53 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3) // error we are using nodejs, not browser
+
+console.log(3+3);
+console.log("KM");
+
+let name = "KM"
+let age = 22
+let isLoggedIn = true
+let state;
+let x = null
+
+
+//number 2^53
+//bigint
+//string " "
+//boolean true/false
+//null
+//undefined
+//symbol unique
+
+
+//object
+
+console.log(typeof "KM");
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
