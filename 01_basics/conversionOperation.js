3285ae008caeb01d1af1167ee414b7680f1e1464
Kaustubh Mishra <83139466+Kaustubh1601@users.noreply.github.com>
2024-02-16 09:49:42 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "32"  
 
-console.log(typeof score);
-console.log(typeof(score));
+////console.log(typeof score);
+////console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+////console.log(typeof valueInNumber);
 
 
 //"32"-> 32
@@ -14,7 +14,7 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+////console.log(booleanIsLoggedIn);
 
 
 //1->true 0->false
@@ -24,5 +24,43 @@ console.log(booleanIsLoggedIn);
 let someNumber = 32
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+
+/************** operations ******************** */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3); % use in crypto algos
+
+
+
+
+let str1 = "hello"
+let str2 = " Km"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(2 + "1");
+//console.log("1" + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+let counter = 100
+counter++;
+console.log(counter);
+
+
