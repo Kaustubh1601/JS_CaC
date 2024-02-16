ebd2bac6f8360f4840b06b862412f700bf64cd76
Kaustubh Mishra <83139466+Kaustubh1601@users.noreply.github.com>
2024-02-16 13:05:39 +0000

    comparison

---
+++ comparison.js
@@ -0,0 +1,20 @@
+//console.log(2>1);
+//>=, <, ==, !=
+
+
+//console.log("2">1); //true
+
+
+//avoid these: clean code
+
+//console.log(null>0); //false NaN
+//console.log(null==0);//false
+//console.log(null>=0);//true
+
+console.log(undefined == 0); //false always<>
+
+//=== strict checking: compare data types!
+// == conversion happens
+console.log("2" === 2); //false
+
+
