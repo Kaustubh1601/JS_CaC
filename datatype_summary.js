3ab75d45661b195411e85f0ad549bd7f96c93d9d
Kaustubh Mishra <83139466+Kaustubh1601@users.noreply.github.com>
2024-02-16 13:34:54 +0000

    Summary

---
+++ datatype_summary.js
@@ -0,0 +1,40 @@
+//primitive
+//: String, Number, null, undefined, Symbol, Boolean, BigInt
+
+
+
+//Non-primitive: Reference
+//Arrays, Objects, Functions
+
+//JS ->static or dynamic?
+
+//const score = 100
+//const scoreValue = 100.3
+
+//const isLoggedIn = false
+//let user; undefined.
+
+const id = Symbol('123')
+const accountId = Symbol('123')
+//Not Equal...===
+
+//const bigNumber = 3235546563233253n;
+
+const heros = ["ironman","thor"]
+
+let myObj = {
+    name: "km",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof(myFunction));
+
+
+//visit ecmaScript!!!!!!!!!!!!
+
+
+
