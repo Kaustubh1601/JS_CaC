2eb0e0baed185b7143c3d8a90373081d31de9ef0
Kaustubh Mishra <83139466+Kaustubh1601@users.noreply.github.com>
2024-04-11 11:46:05 +0000

    03_objects.js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,39 @@
+//singleton
+//Object.create
+
+//symbol
+const mySym = Symbol("key1")
+
+//object literals
+const JsUser = {
+    name : "Kaustubh",
+    Age : 22,
+    email : "km@gmail.com",
+    "status" : "active",
+    [mySym] : "mykey1"  //[] for symbol
+
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["status"])
+console.log(JsUser.mySym)  //string 
+console.log(JsUser[mySym])
+
+JsUser.email ="mishra@google.com"
+Object.freeze(JsUser)
+
+JsUser.email ="mishra@gmaile.com" //wont change
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hi js user");
+}
+
+JsUser.greeting2 = function(){
+    console.log(`hi js user, ${this.name}`);
+}
+
+//console.log(JsUser.greeting); //() 
+console.log(JsUser.greeting()); 
+console.log(JsUser.greeting2());
