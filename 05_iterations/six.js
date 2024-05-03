a85ad62f97bc51888b2e6eeccbb9840126e5719f
Kaustubh Mishra <83139466+Kaustubh1601@users.noreply.github.com>
2024-05-03 15:55:46 +0000

    filer, map and reduce

---
+++ 05_iterations/six.js
@@ -0,0 +1,30 @@
+const coding = ["js", "ruby", "java"]
+
+
+const values = coding.array.forEach( (item) => {
+    console.log(item);     //return 
+    return item
+});
+
+console.log(values); 
+//values will be printed, but undef returned.
+//forEach doesnt return any values.
+
+
+//const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//const newNums = myNums.filter( (num) => num > 4 )       //requires callback fn //returns value
+
+//const newNums = myNums.filter( (num) => {
+  //  'return' num > 4
+// } ) 
+
+const newNums = []
+
+myNums.forEach( (num) => {
+    if(num>4){
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);
